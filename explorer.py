b86bd3c3c731d5c600d18107f7548639621239c1
tianzhao <tianzhao@leoao.com>
2023-05-23 21:32:56 +0800

    根据文件名查阅纪录

--- explorer.py
+++ explorer.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
 
-def view_document(supabase):
-    # Get the document from the database
-    response = supabase.table("documents").select("content").execute()
-    st.write("**This feature is in active development**")
-    # Display a list of elements from the documents
-    # If the user clicks on an element, display the content of the document
-    for document in response.data:
-        if st.button(document['content'][:50].replace("\n", " ")):
+def view_document(client):
+    response = client.table("documents").select("metadata").execute()
+    # 使用列表推导式和集合查找不重复的文件名
+    file_names = list({d['metadata']['file_name'] for d in response.data})
+    # st.write(file_names)
+    for file_name in file_names:
+        if st.button(file_name[:50].replace("\n", " ")):
             continue
